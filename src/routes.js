fb5dfaa5bd16533fe76809019a20cedf44ce3e79
marcelopajr <marcelo96jr@gmail.com>
2023-04-13 15:01:38 -0300

    feat: add user register

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from "express";
-import User from "./app/models/user";
+import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
-routes.get("/test", async (req, res) => {
-  const user = await User.create({
-    name: "Marcelo",
-    email: "teste@gmail.com",
-    password_hash: "123123",
-  });
-  return res.json(user);
-});
+routes.post("/users", UserController.store);
 
 export default routes;
