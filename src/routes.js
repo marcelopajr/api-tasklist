aa168b8c479eb6db3489f49859887a582d392877
marcelopajr <marcelo96jr@gmail.com>
2023-04-24 16:51:20 -0300

    feat: add auth middleware

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
 import { Router } from "express";
+
+import authMiddleware from "./app/middlewares/auth";
+
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 
@@ -7,5 +10,10 @@ const routes = new Router();
 routes.post("/users", UserController.store);
 
 routes.post("/sessions", SessionController.store);
+
+// Check token to all routes below
+routes.use(authMiddleware);
+
+routes.put("/users", UserController.update);
 
 export default routes;
